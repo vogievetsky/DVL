f8b5f73bb0b031abbd8ad480f1c09c358b4e72c5
Vadim Ogievetsky <vadimon@gmail.com>
2012-03-05 15:20:55 -0800

    ported more tests

--- test/core/register.js
+++ test/core/register.js
@@ -15,9 +15,7 @@ suite.addBatch({
 
       dvl.register({
         listen: [t.a],
-        fn: function() {
-          t.runs++;
-        }
+        fn: function() { t.runs++; }
       });
 
       return t;
@@ -27,10 +25,52 @@ suite.addBatch({
       assert.strictEqual(t.runs, 1);
     },
 
-    "correct next run": function(t) {
-      t.a.set(4).notify();
+    "correct next run / dvl.notify()": function(t) {
+      t.a.set(4);
+      dvl.notify(t.a);
       assert.strictEqual(t.runs, 2);
-    }
+    },
+
+    "correct next run / a.set(_).notify()": function(t) {
+      t.a.set(4).notify();
+      assert.strictEqual(t.runs, 3);
+    },
+
+    "correct next run / a.notify()": function(t) {
+      t.a.notify();
+      assert.strictEqual(t.runs, 4);
+    },
+  },
+
+
+  "basic register / const": {
+    topic: function() {
+      var t = {
+        runs: 0,
+        a: dvl.const(3).name('a')
+      }
+
+      dvl.register({
+        listen: [t.a],
+        fn: function() { t.runs++; }
+      });
+
+      return t;
+    },
+
+    "always unchaged": function(t) {
+      assert.strictEqual(t.runs, 1);
+
+      t.a.set(4);
+      dvl.notify(t.a);
+      assert.strictEqual(t.runs, 1);
+
+      t.a.set(4).notify();
+      assert.strictEqual(t.runs, 1);
+
+      t.a.notify();
+      assert.strictEqual(t.runs, 1);
+    },
   },
 
 
@@ -93,6 +133,38 @@ suite.addBatch({
       assert.strictEqual(t.runs, 2);
       assert.strictEqual(t.b.get(), 20);
     }
+  },
+
+
+  "circular register": {
+    topic: function() {
+      var t = {
+        runs: 0,
+        a: dvl.def(3).name('a'),
+        b: dvl.def(null).name('b')
+      }
+
+      dvl.register({
+        listen: [t.a],
+        change: [t.b],
+        fn: function() {
+          t.b.set(t.a.get() * 5).notify();
+          t.runs++;
+        }
+      });
+
+      return t;
+    },
+
+    "cant make circular": function(t) {
+      assert.throws(function() {
+        dvl.register({
+          listen: [t.b],
+          change: [t.a],
+          fn: function() { "whatever" }
+        });
+      });
+    },
   },
 });
 
