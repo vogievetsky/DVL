e5e91eb0deebb8a3410fa8e0f63143d56f90cfca
Cory Barr <cory.barr@gmail.com>
2013-03-05 14:59:44 -0800

    changed camelCase to css-class naming convention, altered long-name entry, removed commented-out code

--- examples/UIComponents/dropdown.js
+++ examples/UIComponents/dropdown.js
@@ -1,22 +1,11 @@
-var COUNTRIES = ["United States", "United Kingdom", "Canada", "Mexico", "England", "Russia", "A Country With A Very Long Name for Testing Purposes"];
-
-/*
-var text = dvl.wrapVar('');
-
-var listParent = d3.select("body").append("div").attr("class", "dropdownParent");
-
-dvl.html.list = dvl.html.list({parent: listParent, 
-				  data: COUNTRIES,
-				  selection: "Canada",
-				  highlight: "Mexico"
-				});
-*/
+var COUNTRIES = [null, "United States", "United Kingdom", "Canada", "Mexico", "England", 
+				"Russia", "The United Kingdom of Great Britain and Northern Ireland"];
 
 var text = dvl.wrapVar('');
 
-var listParent = d3.select("body").append("div").attr("class", "dropdownParent");
+var listParent = d3.select("body").append("div").attr("class", "dropdown-parent");
 
 dvl.html.list = dvl.html.dropdown({parent: listParent, 
 				  data: COUNTRIES,
-				  label: function(d) { return d === null ? "X" : d; }
+				  label: function(d) { return d === null ? "--" : d; }
 				});
