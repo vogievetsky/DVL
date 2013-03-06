1071750424a9478f877c86c763e6adc6f25a48bf
Cory Barr <cory.barr@gmail.com>
2013-03-06 09:25:16 -0800

    added dropdown example with data that flips to null

--- examples/UIComponents/dropdown.js
+++ examples/UIComponents/dropdown.js
@@ -1,11 +1,39 @@
 var COUNTRIES = [null, "United States", "United Kingdom", "Canada", "Mexico", "England", 
 				"Russia", "The United Kingdom of Great Britain and Northern Ireland"];
 
-var text = dvl.wrapVar('');
 
-var listParent = d3.select("body").append("div").attr("class", "dropdown-parent");
-
-dvl.html.list = dvl.html.dropdown({parent: listParent, 
+//example 01
+var parentEx01 = d3.select("div#ex01");
+dvl.html.dropdown({parent: parentEx01, 
 				  data: COUNTRIES,
 				  label: function(d) { return d === null ? "--" : d; }
 				});
+
+//example 02
+var changingData = dvl.wrapVar(null);
+
+setInterval(
+	function () { 
+		var val = changingData.value() === null ? COUNTRIES : null;
+		changingData.value(val); 
+	},
+	1000);
+dvl.debug("changingData", changingData);
+
+var parentEx02 = d3.select("div#ex02");
+
+dvl.bind({
+	parent: parentEx02,
+	self: "div#rawDataOutput",
+	data: changingData,
+	text: function (d) { return d; },
+	style: {
+		border: "solid 1px",
+		width: "100px"
+	}
+});
+
+dvl.html.dropdown({	parent: parentEx02, 
+					data: changingData,
+				  	label: function(d) { return d === null ? "--" : d; }
+					});
